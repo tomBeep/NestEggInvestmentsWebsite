475744f3260388d2e3e6dd74cb932bf9f5620ba3
Thomas Edwards <tomo12321@hotmail.com>
2020-03-02 16:58:16 +1300

    Got Routing working properly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {CarouselModule} from "ngx-bootstrap";
 import {TooltipModule} from "ngx-bootstrap";
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {NavBarComponent} from './shared-components/nav-bar/nav-bar.component';
+import {ScrollingBannerComponent} from './pages/home/scrolling-banner/scrolling-banner.component';
+import {BookNowBannerComponent} from './pages/home/book-now-banner/book-now-banner.component';
+import {BannerItemComponent} from './pages/home/scrolling-banner/banner-item/banner-item.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NavBarComponent } from './pages/home/nav-bar/nav-bar.component';
-import { ScrollingBannerComponent } from './pages/home/scrolling-banner/scrolling-banner.component';
-import { BookNowBannerComponent } from './pages/home/book-now-banner/book-now-banner.component';
-import { BannerItemComponent } from './pages/home/scrolling-banner/banner-item/banner-item.component';
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'services', component: BookNowBannerComponent},
+  {path: 'client-references', component: BookNowBannerComponent},
+  {path: 'blog', component: BookNowBannerComponent},
+  {path: 'about', component: BookNowBannerComponent},
+  {path: 'investment-principals', component: BookNowBannerComponent},
+  {path: 'contact', component: BookNowBannerComponent},
+  {
+    path: 'test123',
+    component: BookNowBannerComponent,
+    data: {title: 'Heroes List'}
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {path: '**', component: BookNowBannerComponent} //Page Not Found Component
+];
 
 @NgModule({
   declarations: [
@@ -21,8 +41,11 @@ import { BannerItemComponent } from './pages/home/scrolling-banner/banner-item/b
     BannerItemComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      // {enableTracing: true} // <-- debugging purposes only
+    ),
     BrowserModule,
-    AppRoutingModule,
     CarouselModule.forRoot(),
     TooltipModule.forRoot(),
   ],
@@ -30,4 +53,5 @@ import { BannerItemComponent } from './pages/home/scrolling-banner/banner-item/b
   bootstrap: [AppComponent]
 })
 
-export class AppModule {}
+export class AppModule {
+}
--- src/app/shared-components/nav-bar/nav-bar.component.ts
+++ src/app/shared-components/nav-bar/nav-bar.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  currentPage : string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setCurrentPage(newPage:string){
+    console.log("Setting page: "+newPage)
+    this.currentPage = newPage;
   }
 
 }
